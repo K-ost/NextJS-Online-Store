6e4c9d2c976322662503688d73c6c1fe5fbb5a6e
K-ost <golubkovru@gmail.com>
2022-07-30 13:16:12 +0300

    helpers.ts

--- helpers.ts
+++ helpers.ts
@@ -1,2 +1,2 @@
-export const server = 'https://nextjs-online-store.herokuapp.com/'
+export const server = 'https://nextjs-online-store.herokuapp.com'
 export const oldServer = 'http://localhost:5000'
